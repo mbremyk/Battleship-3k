9087e9d78b25afeab2af8c4fb41630b4c95211e5
trymg <trymg@stud.ntnu.no>
2019-03-14 18:35:32 +0100

    commenting

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,9 +1,11 @@
 package game;
+import model.BattleshipUser;
 
 /**
  * two boards
  * what players are playing
  * graphical logic
+ * communication with controller(game looks), dbconstants, battleshipuser, dbc
  */
 public class Game {
 }
