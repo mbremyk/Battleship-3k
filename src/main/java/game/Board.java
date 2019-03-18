c0c2736f9f2e2a711d2953c8223be17d786bc822
tdthorki <tdthorki@stud.ntnu.no>
2019-03-18 19:14:10 +0100

    Added method for saving a ships position from array in Board.java, with a small test client

--- src/main/java/game/Board.java
+++ src/main/java/game/Board.java
@@ -1,6 +1,9 @@
 package game;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.util.Arrays;
 
 /**
  * info on where objects are located on the board
@@ -12,14 +15,53 @@ public class Board extends ImageView {
     int[][] board;
 
 
-    public Board(){
-        super();
+    public Board() {
+//        super(new Image("./grid10x10.png"));
         board = new int[10][10];
+    }
+
+    public void saveShipPositions(int[][] pos) {
+        for (int i = 0; i < pos.length; i++) {
+            board[pos[i][0]][pos[i][1]] = 1;
+        }
     }
 
     public boolean attack(int x, int y) {
         //checks if defenders board has a ship at given coordinates
 
         return false;
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+
+        for (int j = 0; j < board[0].length; j++) {
+            ret += "|";
+            for (int i = 0; i < board.length; i++) {
+                ret += board[i][j] + ((i == board.length - 1) ? "" : ",");
+            }
+            ret += "|\n";
+        }
+
+        return ret;
+    }
+
+    /**
+     * Test client
+     * @param args
+     */
+
+    public static void main(String[] args){
+        Board board = new Board();
+        int[][] testPos = {
+                {0,4},
+                {1,4},
+                {2,4},
+                {6,6},
+                {6,7},
+        };
+        board.saveShipPositions(testPos);
+        System.out.println(board);
     }
 }
